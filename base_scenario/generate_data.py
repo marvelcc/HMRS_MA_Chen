6f401338231c6d06418011add34c68ca501a88b2
marvelcc <chaolong.c@gmail.com>
2023-09-17 05:16:36 +0200

    completed base scenario, added co2 cap scenario

--- base_scenario/generate_data.py
+++ base_scenario/generate_data.py
@@ -17,94 +17,94 @@ T = 12
 
 # -------------------------------------------------
 
-# Activation cost of each facility
+# # Activation cost of each facility
 
-C_f = {a: round(random.normalvariate(20, 2)) * 100000 for a in manu}
-C_g = {a: round(random.normalvariate(20, 2)) * 100000 for a in remanu}
-C_h = {a: round(random.normalvariate(20, 2)) * 100000 for a in hybrid}
-C_j = {a: round(random.normalvariate(40, 4)) * 10000 for a in collect}
-C_u = {a: round(random.normalvariate(10, 1)) * 100000 for a in disassembly}
-C_v = {a: round(random.normalvariate(80, 8)) * 10000 for a in recycling}
-C_w = {a: round(random.normalvariate(40, 4)) * 10000 for a in disposal}
+# C_f = {a: round(random.normalvariate(20, 2)) * 100000 for a in manu}
+# C_g = {a: round(random.normalvariate(20, 2)) * 100000 for a in remanu}
+# C_h = {a: round(random.normalvariate(20, 2)) * 100000 for a in hybrid}
+# C_j = {a: round(random.normalvariate(40, 4)) * 10000 for a in collect}
+# C_u = {a: round(random.normalvariate(10, 1)) * 100000 for a in disassembly}
+# C_v = {a: round(random.normalvariate(80, 8)) * 10000 for a in recycling}
+# C_w = {a: round(random.normalvariate(40, 4)) * 10000 for a in disposal}
 
-locations = {**C_f, **C_g, **C_h, **C_j, **C_u, **C_v, **C_w}
+# locations = {**C_f, **C_g, **C_h, **C_j, **C_u, **C_v, **C_w}
 
-with open('base_scenario/activation_cost.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    for key, value in locations.items():
-        writer.writerow([key, value])
+# with open('base_scenario/activation_cost.csv', 'w') as csv_file:
+#     writer = csv.writer(csv_file)
+#     for key, value in locations.items():
+#         writer.writerow([key, value])
 
-csv_file.close()
+# csv_file.close()
 
 # -------------------------------------------------
 
-# Production/processing cost for each type of facility
+# # Production/processing cost for each type of facility
 
-B_F_t = []
-B_H_t = []
-B_G_t = []
-B_H_bar_t = []
-B_J_t = []
-B_KU_t = []
-B_U_t = []
-B_V_t = []
-B_W_t = []
+# B_F_t = []
+# B_H_t = []
+# B_G_t = []
+# B_H_bar_t = []
+# B_J_t = []
+# B_KU_t = []
+# B_U_t = []
+# B_V_t = []
+# B_W_t = []
 
-for t in range(T):
+# for t in range(T):
     
-    B_F = random.randrange(20,40)
-    B_G = random.randrange(10,20)
-    B_H = random.randrange(20,40)
-    B_H_bar = random.randrange(10,20)
-    B_J = random.randrange(4,8)
-    B_KU = random.randrange(2,4)
-    B_U = random.randrange(10,20)
-    B_V = random.randrange(8,16)
-    B_W = random.randrange(4,8)
+#     B_F = random.randrange(20,40)
+#     B_G = random.randrange(10,20)
+#     B_H = random.randrange(20,40)
+#     B_H_bar = random.randrange(10,20)
+#     B_J = random.randrange(4,8)
+#     B_KU = random.randrange(2,4)
+#     B_U = random.randrange(10,20)
+#     B_V = random.randrange(8,16)
+#     B_W = random.randrange(4,8)
 
     
-    B_F_t.append(B_F)
-    B_H_t.append(B_H)
-    B_G_t.append(B_G)
-    B_H_bar_t.append(B_H_bar)
-    B_J_t.append(B_J)
-    B_KU_t.append(B_KU)
-    B_U_t.append(B_U)
-    B_V_t.append(B_V)
-    B_W_t.append(B_W)
+#     B_F_t.append(B_F)
+#     B_H_t.append(B_H)
+#     B_G_t.append(B_G)
+#     B_H_bar_t.append(B_H_bar)
+#     B_J_t.append(B_J)
+#     B_KU_t.append(B_KU)
+#     B_U_t.append(B_U)
+#     B_V_t.append(B_V)
+#     B_W_t.append(B_W)
 
-processing_cost = {'B_F': B_F_t, 'B_G': B_G_t, 'B_H': B_H_t, 'B_H_bar': B_H_bar_t, 'B_J': B_J_t, 'B_KU': B_KU_t, 'B_U': B_U_t, 'B_V': B_V_t, 'B_W': B_W_t}
-df_processing = pd.DataFrame(processing_cost)
-df_processing.to_csv('base_scenario/processing_cost.csv')
-
-# -------------------------------------------------
-
-# Procurement cost of each component at each period
-
-price_ranges = {'k1': (6, 10), 'k2': (4, 8), 'k3': (1, 3)}
-df_procurement = pd.DataFrame()
-for k in component:
-    min_price, max_price = price_ranges[k]
-    prices = [random.randint(min_price, max_price) for _ in range(T)]
-    df_procurement[k] = prices
-df_procurement.to_csv('base_scenario/procurement_price.csv')
+# processing_cost = {'B_F': B_F_t, 'B_G': B_G_t, 'B_H': B_H_t, 'B_H_bar': B_H_bar_t, 'B_J': B_J_t, 'B_KU': B_KU_t, 'B_U': B_U_t, 'B_V': B_V_t, 'B_W': B_W_t}
+# df_processing = pd.DataFrame(processing_cost)
+# df_processing.to_csv('base_scenario/processing_cost.csv')
 
 # -------------------------------------------------
 
-# Create distance matrix between each location, not all combination are used
+# # Procurement cost of each component at each period
 
-locations = supplier + manu + remanu + hybrid + warehouse + collect + disassembly + recycling + disposal
-distance_matrix = pd.DataFrame(np.nan, columns=locations, index=locations)
-for location1 in locations:
-    for location2 in locations:
-        if location1 == location2:
-            distance_matrix.at[location1, location2] = 0.0
-        else:
-            # Generate a random distance between 100 and 350
-            distance = np.random.randint(10, 35) * 10
-            distance_matrix.at[location1, location2] = distance
-            distance_matrix.at[location2, location1] = distance
-distance_matrix.to_csv('base_scenario/distance_matrix.csv')
+# price_ranges = {'k1': (6, 10), 'k2': (4, 8), 'k3': (1, 3)}
+# df_procurement = pd.DataFrame()
+# for k in component:
+#     min_price, max_price = price_ranges[k]
+#     prices = [random.randint(min_price, max_price) for _ in range(T)]
+#     df_procurement[k] = prices
+# df_procurement.to_csv('base_scenario/procurement_price.csv')
+
+# -------------------------------------------------
+
+# # Create distance matrix between each location, not all combination are used
+
+# locations = supplier + manu + remanu + hybrid + warehouse + collect + disassembly + recycling + disposal
+# distance_matrix = pd.DataFrame(np.nan, columns=locations, index=locations)
+# for location1 in locations:
+#     for location2 in locations:
+#         if location1 == location2:
+#             distance_matrix.at[location1, location2] = 0.0
+#         else:
+#             # Generate a random distance between 100 and 350
+#             distance = np.random.randint(10, 35) * 10
+#             distance_matrix.at[location1, location2] = distance
+#             distance_matrix.at[location2, location1] = distance
+# distance_matrix.to_csv('base_scenario/distance_matrix.csv')
 
 # -------------------------------------------------
 
